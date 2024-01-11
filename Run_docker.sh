7b1af8a8e003344cde5f8a4fea26bfab05c2ea17
Gabriel Fischer Abati <39131927+git-gfischer@users.noreply.github.com>
2024-01-11 12:09:31 +0100

    Update Run_docker.sh

--- Run_docker.sh
+++ Run_docker.sh
@@ -1,3 +1,3 @@
 xhost +
-sudo nvidia-docker run -ti -v /tmp/.X11-unix:/tmp/.X11-unix --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -e DISPLAY=$DISPLAY -v $(pwd)/Dataset:/home/Dataset --name=dyna --gpus all dyna bash
+sudo docker run -ti -v /tmp/.X11-unix:/tmp/.X11-unix --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -e DISPLAY=$DISPLAY -v $(pwd)/Dataset:/home/Dataset --name=dyna --gpus all dyna bash
 
